6aa6f6f608d418c2f5bcba5b5c8180a5dc4e900c
WorKai7 <jvcraft59@gmail.com>
2026-01-18 17:49:32 +0100

    feat(doc): Finished API and added docs at /api-docs

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,12 +2,19 @@ import express from 'express'
 import {userRouter} from './user.route.js'
 import { shroomRouter } from "./shroom.route.js"
 import { authRouter } from "./auth.route.js"
+import swaggerUi from 'swagger-ui-express'
+import {swaggerDocument} from './docs/index.js'
 
 export const app = express()
 
 const port = 3000
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "Shroompedia API Documentation"
+}))
 
 app.get('/', (_req, res) => {
     res.status(200).send('Bienvenue sur le serveur HTTP')
